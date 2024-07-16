22406a203899ac86ccb379763dee16ee76984cca
Chafe Solomon <chafe@laptop86.tresata.com>
2024-07-16 17:07:17 -0400

    added param support for script

--- aws/Capstone Project: CloudUploader CLI/project.sh
+++ aws/Capstone Project: CloudUploader CLI/project.sh
@@ -1,10 +1,46 @@
 #!/bin/bash
+cloud=$1
+file=$2
 
 # Check if any cli is missing and if it is then install it. 
 for app in aws az; do
-    $app --version &> /dev/null
-    if [ $? -gt 0 ]; then
-        echo -e "Missing $app cli... please install before continuing"
+
+    if ! command -v "$app" &> /dev/null ; then
+        echo "Missing $app CLI. Please install it before continuing."
         exit 1
     fi
 done
+
+# Choose cloud platform
+if [ -z $cloud ]; then
+    echo 'Please enter which cloud provider you are using. AWS or Azure?'
+    exit 1
+fi
+# Setup & Authentication:
+
+# AWS
+if [[ $cloud == 'aws' ]]; then
+    aws(){
+        if [ ! -f $HOME/.aws/credentials ]; then
+            echo 'Please create credentials for AWS'
+            exit 1
+        fi
+        echo 'creds exist'
+    }
+
+# Azure
+elif [[ $cloud == 'az' ]]; then
+
+    azure(){
+        az login
+    }
+
+else
+    echo 'please run again using aws or az'
+fi
+
+# CLI Argument Parsing
+
+# File Check
+
+# File Upload
