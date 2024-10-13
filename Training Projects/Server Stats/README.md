317ae83bdf28226a3c2c59016a9148b66fdfb038
Chafe Solomon <62526967+ChafeSolomon@users.noreply.github.com>
2024-10-13 16:29:30 -0400

    Squashed last 8 commits into this one

---
+++ Training Projects/Server Stats/README.md
@@ -0,0 +1,4 @@
+Project test is from [roadmap.sh](https://roadmap.sh/projects/server-stats)
+
+<h1>System Resource Monitoring Script</h1>
+This Bash script provides a summary of the system's resource usage, including CPU, RAM, and storage. It also displays the top 5 processes consuming the most CPU and memory. The script is designed to be run on a Linux-based system with access to tools such as top, ps, and df.
--- Training Projects/Server Stats/server-stats.sh
+++ Training Projects/Server Stats/server-stats.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
-echo -e "Cpu Usage\n"
-top -n 1 | grep ^%Cpu
+cpu_usage (){
+  echo -e "\nCpu Usage\n"
+  top -n 1 | grep ^%Cpu
+}
 
-echo -e "Ram Usage\n"
-top -n 1 | grep ^MiB\ Mem
+ram_usage (){
+  echo -e "\nRam Usage\n"
+  top -n 1 | grep ^MiB\ Mem
+}
 
-echo -e "Storage Usage\n"
-df -h
+storage_usage (){
+  echo -e "\nStorage Usage\n"
+  df -h
+}
 
-echo -e "Top 5 processes by CPU usage\n"
-ps aux --sort -%cpu | head -n 5
-echo -e "Top 5 processes by memory usage\n"
-ps aux --sort -%mem
+five_cpu() {
+  echo -e "\nTop 5 processes by CPU usage\n"
+  ps aux --sort -%cpu | head -n 5
+}
+
+five_mem() {
+
+  echo -e "\nTop 5 processes by memory usage\n"
+  ps aux --sort -%mem
+}
+
+for i in cpu_usage ram_usage storage_usage five_cpu five_mem; do
+  $i
+done
